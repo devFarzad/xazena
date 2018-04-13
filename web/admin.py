dd20453e4e3d2d90d01bebe8b380c2414a99dcd8
the.Farshad <farshad.1991@gmail.com>
2018-04-13 13:35:59 +0430

    income models

--- web/admin.py
+++ web/admin.py
@@ -3,7 +3,8 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import Expense
+from .models import Expense, Income
 
 # Register your models here.
 admin.site.register(Expense)
+admin.site.register(Income)
